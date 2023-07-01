d967600d4c3976802b8e68ae140702d95f952b08
andrewbocker <58228967+andrewbocker@users.noreply.github.com>
2023-06-30 19:01:32 -0700

    commit inicial proyecto

---
+++ tiendaProyecto/src/main/java/com/tiendaProyecto/TiendaProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaProyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaProyectoApplication.class, args);
+	}
+
+}
